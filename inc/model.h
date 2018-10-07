7ff4368ca9ccf54d71b9f3fac2a828e9ad67ed03
fabricio.hmf <fabricio.hmf@da5e9a1c-67a0-4f10-8a01-8bc282ad989b>
2018-10-07 04:09:57 +0000

    >model creator

--- inc/model.h
+++ inc/model.h
@@ -12,17 +12,28 @@
 
 namespace Model
 {
-	// domain partition for fracture generation
 	struct DemBox
 	{
-	    Vector3 pos;
-	    double zbase;
-	    double dx;
-	    double dy;
-	    int matid;
-	    DemBox():pos(0.0),zbase(0.0),dx(0.0),dy(0.0),matid(0){}
+		Vector3 pos;
+		double zbase;
+		double dx,dy,dz;
+		int matid;
+		DemBox():pos(0.0),zbase(0.0),dx(0.0),dy(0.0),dz(0.0),matid(0){}
 	};
+
+	struct Particle
+	{
+		Vector3 pos;
+		double vol;
+		double lp;
+		int matid;
+		Particle():pos(0.0),vol(0.0),lp(0.0),matid(0){}
+	};
+	
 	std::vector<DemBox>& GetDemVector();
+	std::vector<Particle>& GetParticleVector();
+	
+	void CreateMPMmodel();
 }
 
 #endif /* INC_MODEL_H_ */
