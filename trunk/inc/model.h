88df05791f0fa7d4cec968f2bcf1c1148582a7d0
fabricix <fabricix@da5e9a1c-67a0-4f10-8a01-8bc282ad989b>
2018-11-23 14:57:36 +0000

    implementação particulas por grade

--- trunk/inc/model.h
+++ trunk/inc/model.h
@@ -44,15 +44,34 @@ namespace Model
 	{
 		int id;
 		int bndry;
+		int mat;
 		std::vector<int> points;
+	};
+
+	struct mshGrid
+	{
+		Vector3 regionBegin;       		// minimum x,y,z of region
+		Vector3 regionEnd;         		// maximum x,y,z of region
+		double dx;                 		// x-dir cell size
+		double dy;                 		// y-dir cell size
+		double dz;                 		// z-dir cell size
+		int Ng;  	              		// number of rings of nodes outside region
+		int I;                     		// number of grid nodes in x direction
+		int J;                     		// number of grid nodes in y direction
+		int K;                     		// number of grid nodes in z direction
+		int Nnodes;
+		std::vector<Vector3> gnodes;	// grid nodes
 	};
 
 	std::vector<DemBox>& GetDemVector();
 	std::vector<Particle>& GetParticleVector();
 	std::vector<mshPoint>& GetmshPointsVector();
 	std::vector<mshElement>& GetmshElementsVector();
+	mshGrid& GetmshGrid();
 	
 	void CreateMPMmodel();
+	void initGrid(Vector3 limmin,Vector3 limmax, Vector3 Ncell);
+
 }
 
 #endif /* INC_MODEL_H_ */
